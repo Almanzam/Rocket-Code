45c7750582590ce6d377cfebf533fd8d4e43068b
Almanzam <Almanzam@vt.edu>
2016-02-11 17:47:35 -0500

    new better stuff

--- dataloggerexample/Datalogger.ino
+++ dataloggerexample/Datalogger.ino
@@ -46,21 +46,30 @@ void setup() {
 
 void loop() {
   // make a string for assembling the data to log:
-  String dataString = "";
-
+  String accelString = "";
+  String baroString = "";
+  String gyroString = "";
+  int loopcount = 0;
   // read three sensors and append to the string:
-  for (int analogPin = 0; analogPin < 3; analogPin++) {
-    int sensor = analogRead(analogPin);
-    dataString += String(sensor);
-    if (analogPin < 2) {
-      dataString += ",";
-    }
-  }
+//  for (int analogPin = 0; analogPin < 3; analogPin++) {
+//    int sensor = analogRead(analogPin);
+//    dataString += String(sensor);
+//    if (analogPin < 2) {
+//      dataString += ",";
+//    }
+//  }
+  int accel = analogread(0);
+  int baro = analogread(1);
+  int gyro = analogread(2);
+
+  accelString += String(accel);
+  baroString += String(baro);
+  gyroString += String(gyro);
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
+  File accelerometer = SD.open("accellog.txt", FILE_WRITE);
+  
   // if the file is available, write to it:
   if (dataFile) {
     dataFile.println(dataString);
@@ -68,6 +77,15 @@ void loop() {
     // print to the serial port too:
     Serial.println(dataString);
   }
+  File barometer = SD.open("barolog.txt", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  File gyroscope = SD.open("gyrolog.txt", FILE_WRITE);
+
   // if the file isn't open, pop up an error:
   else {
     Serial.println("error opening datalog.txt");
